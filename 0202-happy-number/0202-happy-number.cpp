9ab93019930291a4e4ff112565f5afecafbc565c
Aniket Kumar Singh <aniketyt2503@gmail.com>
2026-09-07 14:46:56 +0530

    Time: 0 ms (100%), Space: 7.9 MB (67.18%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int squareNum(int n){
+        int ans = 0;
+        while(n!=0){
+            ans += (n%10) * (n%10);
+            n /= 10;
+        }
+        return ans;
+    }
+    bool isHappy(int n) {
+        int a = n;
+        int b = squareNum(n);
+        while (b!=1 && a!=b){
+            a = squareNum(a);
+            b = squareNum(squareNum(b));
+        }
+        return b==1;
+    }
+};
