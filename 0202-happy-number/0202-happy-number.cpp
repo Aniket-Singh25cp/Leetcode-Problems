9fe65946ecab0b8bdc41ed9c1db836d8f51df6d7
Aniket Kumar Singh <aniketyt2503@gmail.com>
2026-09-07 14:48:51 +0530

    Time: 0 ms (100%), Space: 7.8 MB (82.4%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -13,7 +13,10 @@ public:
         int b = squareNum(n);
         while (b!=1 && a!=b){
             a = squareNum(a);
-            b = squareNum(squareNum(b));
+            b = squareNum(squareNum(b)); //Floyd's Cycle FInding Algorithm
+            // if a nummber is stuck in loop; a moves 1 step and b moves 2 step
+            // if they are in a loop then b will crash into a 
+            //Also called the Hare-Tortoise Algorithm
         }
         return b==1;
     }
