944e417ed21374ae37f3ac5e54a2d9ff125f63cd
Aniket Kumar Singh <aniketyt2503@gmail.com>
2026-09-08 11:21:57 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.49%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int ans = 0;
-        if(n < 1000) return 0;
-        if(n == 100000) return 99001;
-        if(n >= 1000 && n<=99999){
-            ans += (n - 1000 + 1);
-        }
-        return ans;
+        return n < 999 ? 0 : n - 1000 + 1;
     }
 };
