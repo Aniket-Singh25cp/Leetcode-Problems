22622846cd88416b4eea37d89d1d953d0ff19544
Aniket Kumar Singh <aniketyt2503@gmail.com>
2026-09-10 15:54:24 +0530

    Time: 0 ms (100%), Space: 8.6 MB (48.56%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+    int gcdd(int a, int b){
+        return b == 0 ? a : gcdd(b, a%b);
+    }
     int gcdOfOddEvenSums(int n) {
         int a = n*n;
         int b = n*(n+1);
-        return gcd(a,b);
+        return gcdd(a,b);
     }
 };
