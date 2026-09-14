5c3540e7542862d1499c7e11deb3363903a924e1
Aniket Kumar Singh <aniketyt2503@gmail.com>
2026-09-14 12:46:45 +0530

    Time: 0 ms (100%), Space: 42.1 MB (73.52%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,11 +1,5 @@
 class Solution {
     public int numberOfSteps(int n) {
-        int c = 0;
-        while(n > 0){
-            if(n % 2 == 0) n /= 2;
-            else n -= 1;
-            c++;
-        }
-        return c;
+        return n > 0 ? Integer.bitCount(n) + 31 - Integer.numberOfLeadingZeros(n) : 0;
     }
 }
