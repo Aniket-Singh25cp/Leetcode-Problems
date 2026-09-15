cb3e26975c7453526ad4126647b448728819ff0a
Aniket Kumar Singh <aniketyt2503@gmail.com>
2026-09-15 11:18:56 +0530

    Time: 0 ms (100%), Space: 41.7 MB (93.23%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,15 @@
 class Solution {
-    public int fbi(int n){
-        if(n == 0 || n == 1) return n;
-        return fbi(n - 1) + fbi(n - 2);
-    }
+    // public int fbi(int n){
+    //     if(n == 0 || n == 1) return n;
+    //     return fbi(n - 1) + fbi(n - 2);
+    // }
     public int fib(int n) {
-        return fbi(n);
+        int arr[] = new int[n + 2];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i = 2; i < n + 1; i++){
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
     }
 }
